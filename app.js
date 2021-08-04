9df4b80c696312158f6887f354668f1225b7209e
zeecode90 <mohdzeeshankhan982@gmail.com>
2021-08-04 11:27:43 -0700

    objects and loops added

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var b = 10;
 var c = a+b;
 console.log('Sum = ', c);
 
+var obj = {
+    "name" : "Syed Mohammad Asad",
+    "age" : 21,
+    "Address" : "Baxipur, Gorakhpur"
+}
+
+console.log('Object------>', obj.age);
+
+for(var i=0; i<10; i++){
+    console.log(i);
+}
 
 //git add .
 //git commit -m "any message"
